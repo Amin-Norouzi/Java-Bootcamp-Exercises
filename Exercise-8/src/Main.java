87818054998dddf555112a6964089c5684343030
Amin Norouzi <aminnorouzidev@gmail.com>
2022-03-09 16:39:21 +0330

    Add files via upload

---
+++ Exercise-8/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Answer Q1
+        /*
+        SOAP vs REST:
+
+        - SOAP stands for Simple Object Access Protocol whereas REST stands for Representational State Transfer.
+        - SOAP is a protocol whereas REST is an architectural pattern.
+        - SOAP uses service interfaces to expose its functionality to client applications while REST uses Uniform
+          Service locators to access to the components on the hardware device.
+        - SOAP needs more bandwidth for its usage whereas REST doesn't need much bandwidth.
+        - Comparing SOAP vs REST API, SOAP only works with XML formats whereas REST work with plain text, XML,
+          HTML and JSON.
+        - SOAP cannot make use of REST whereas REST can make use of SOAP.
+        - REST uses easy to understand standards like swagger and OpenAPI Specification 3.0 and requires use of HTTP.
+        - SOAP has built-in error handling.
+        - SOAP provides significant pre-build extensibility in the form of the WS* standards
+         */
+
+        // Answer Q2
+        /*
+        HTTP request methods:
+
+        - GET: The GET method requests a representation (data) of the specified resource. Requests using GET should
+          only retrieve data.
+        - POST: The POST method submits an entity to the specified resource, often causing a change (create/update)
+          in state or side effects on the server.
+        - PUT: The PUT method replaces all current representations of the target resource with the request payload.
+        - DELETE: The DELETE method deletes the specified resource.
+         */
+
+    }
+}
